a14b800446067d582b664d3b85571aba9ccada71
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-02-20 16:15:47 +0100

    version 2.2.0
    Programme pour calculer la solution de l'équation de second degré

--- C++/pp/main.cpp
+++ C++/pp/main.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-
  int main(){
-   cout<<"donner un number:";
-   int x;
-   cin>>x;
-   cerr<<"votre number est:"<<x;
+  cout <<"=================si un programe qui calculer la solution de equation de 2eme degrée================="<<endl;
+   double a,b,c;
+    cout<<"donner les valeur a b c respectivment:"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+ double D = b*b-4*a*c;
+   if(D>0){
+        double x1=(-b+sqrt(D)/2*a);
+        double x2=(-b-sqrt(D)/2*a);
+    cout<<"les solution est: X1="<<x1<<"X2="<<x2 <<endl;
+   }
+   else if(D<0){
+       double f = -b/2*a;
+    cout<<"la solution est:"<<f<<endl;
+   }
+   else{
+      cout<<"pas de solutions dans IR"<<endl;
+   }
  }
