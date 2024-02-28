7c384c8f0addca5822091a12f952673600766870
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-02-28 13:12:56 +0100

    .

---
+++ C++/TP1_07.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+    {
+        int s =0;
+        int count = 0;
+        int nbre;
+        while (true)
+        {
+           for( int i = 0 ; i <)
+        }
+        
+        
+    }
