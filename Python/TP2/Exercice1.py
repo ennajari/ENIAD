44da63763cbb285d4c5a9feba96aea1a316f73f8
Abdellah Ennajari <105159582+ennajari@users.noreply.github.com>
2024-03-01 14:35:14 +0100

    03/10/24

---
+++ Python/TP2/Exercice1.py
@@ -0,0 +1,13 @@
+import math
+def cube(x):
+    return x*x*x
+
+def volume_Sphere (vol):
+     v = 4/3*math.pi*cube(n)
+     print(f"le volume est:\t{v} \n")
+
+while(True):
+    n = int(input("\ndonner un number : "))
+    print(f"la cube de ce number est :\t {cube(n)}\n")
+    vol = cube(n)
+    volume_Sphere(n)
