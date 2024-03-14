ffce52432608e1505a95e435e3c34de3332f43a0
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-14 00:08:03 +0000

    Somme_des_elements

---
+++ C++/TP3/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int Somme_des_elements(int table[],int taille);
+
+int main(){
+    int taille;
+ cout<<"donner la taille de votre tableau : "<<endl;
+  cin>>taille;
+  int table[taille];
+  int resulta = Somme_des_elements(table , taille);
+  cout << "La somme des elements du tableau est : "<<resulta<<endl;
+
+  
+}
+int Somme_des_elements(int table,int taille){
+    int  somme=0;
+    for ( int i = 0 ; i<=taille ; i++){
+        cout<<"veullez taper l'element  numero "<<i+1<<": ";
+      	cin>>table[i];
+        somme+=(table[i]);
+    }
+    return  somme;
+}
