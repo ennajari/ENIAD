5b680735fc5a7bd11209652aa4cee506b3edd00e
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-15 02:38:07 +0000

    5.cpp / vector & produit scaliare

--- C++/TP3/5.cpp
+++ C++/TP3/5.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main()
 {
-    
+    int taille;
+    int vector<int> vect1;
+    cout << "Donner la taille du tableau : ";
+    cin >> taille;
+    for(int i = 0 ; i < taille ; i++){
+        cout << "Taper elemnt"<<i+1<<" de vecteur 1 :" ;
+        vect1.push_back(elemnt)
+    }
+     vector<int>vect2;
+    for(int i = 0 ; i < taille ; i++){
+        cout << "Taper elemnt"<<i+1<<" de vecteur 2 :" ;
+        vect2.push_back(elemnt)
+    }
+    vector<int>vect3(taille);
+    for(int i = 0 ; i < taille ; i++){
+       vect3 = vect1[i]*vect2[i];
+    }
+    //Affichage des resultats
+    for (size_t i = 0; i < taille; i++)
+    {
+       cout<<vect3;
+    }
+    return 0;
 }
