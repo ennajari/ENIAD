3e3039a1c1ca7014bfdd85d296ffec15f9d15832
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-15 01:44:20 +0000

    4.cpp/vector

--- C++/TP3/4.cpp
+++ C++/TP3/4.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int man()
- {
-    int taille;
-    cout << "Donner la taile  de l'image : ";
-    
-}
+int main() {
+    int taille1;
+    cout << "Donner la taille du tableau 1 : ";
+    cin >> taille1;
+    vector<int> tableau1;
+    for(int i = 0; i < taille1; i++) {
+        int element;
+        cout << "Taper element N" << i + 1 << ": ";
+        cin >> element;
+        tableau1.push_back(element);
+    }
 
+    int taille2;
+    cout << "Donner la taille du tableau 2 : ";
+    cin >> taille2;
+    vector<int> tableau2;
+    for(int j = 0; j < taille2; j++) {
+        int element;
+        cout << "Taper element N" << j + 1 << ": ";
+        cin >> element;
+        tableau2.push_back(element);
+    }
+
+    vector<int> tableau3;
+    for(int k = 0; k < taille1; k++) {
+        tableau3.push_back(tableau1[k]);
+    }
+    for(int k = 0; k < taille2; k++) {
+        tableau3.push_back(tableau2[k]);
+    }
+
+    cout << "Le tableau 3 après la concaténation : ";
+    for(int k = 0; k < tableau3.size(); k++) {
+        cout << tableau3[k] << " ";
+    }
+
+    return 0;
+}
