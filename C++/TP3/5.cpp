480f697705d5e385ae5a6a7f0b5ca360724bab9e
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-15 02:48:19 +0000

    vecteur1 & vecteur 2

--- C++/TP3/5.cpp
+++ C++/TP3/5.cpp
@@ -4,26 +4,30 @@ using namespace std;
 int main()
 {
     int taille;
-    int vector<int> vect1;
-    cout << "Donner la taille du tableau : ";
+     vector<int> vect1;
+    cout << "Donner la taille du vecteur 1 : ";
     cin >> taille;
     for(int i = 0 ; i < taille ; i++){
+        int elemnt;
         cout << "Taper elemnt"<<i+1<<" de vecteur 1 :" ;
-        vect1.push_back(elemnt)
+        cin>>elemnt;
+        vect1.push_back(elemnt);
     }
      vector<int>vect2;
     for(int i = 0 ; i < taille ; i++){
+        int elemnt;
         cout << "Taper elemnt"<<i+1<<" de vecteur 2 :" ;
-        vect2.push_back(elemnt)
+        cin>>elemnt;
+        vect2.push_back(elemnt);
     }
     vector<int>vect3(taille);
     for(int i = 0 ; i < taille ; i++){
-       vect3 = vect1[i]*vect2[i];
+       vect3 += vect1[i] * vect2[i];
     }
     //Affichage des resultats
     for (size_t i = 0; i < taille; i++)
     {
-       cout<<vect3;
+       cout <<vect3[i]<<endl;
     }
     return 0;
 }
