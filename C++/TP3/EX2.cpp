7e5b7f8058c4b9fb989f555c680fb75fbbfc90f6
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-16 02:20:23 +0000

    TP3_C++

--- C++/TP3/EX2.cpp
+++ C++/TP3/EX2.cpp
@@ -9,8 +9,25 @@ private :
        string auteur;
        int  Annepublication; 
 public :
-Livre(string _titre , string _auteur , int _Annepublication : titre(_titre) , auteur(_auteur) ,Annepublication(_Annepublication)){}
-string getTitre()
+Livre(String _titre , string _auteur , int _Annepublication ) : titre(_titre) , auteur(_auteur) , Annepublication(_Annepublication) {}
+string getTitre() const{
+    return titre;
+}
+string getauteur() const{
+    return auteur;
+}
+int getAnnepublication() const{
+    return Annepublication;
+}
+void setTitre(string _titre){
+    titre = _titre;
+}
+void setAnnepublication(string _Annepublication){
+    titre = _Annepublication;
+}
+void setauteur(string _auteur){
+    titre = _auteur;
+}
 };
 
 class Bibliotheque{
