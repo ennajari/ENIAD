0fa96656c3a54bb9aa84b5fdaf5e75e12f508536
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-16 01:59:27 +0000

    TP3 OOP CPP

---
+++ C++/TP3/EX1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Persone {
+    private :
+            string nom;
+            int age;
+    public :
+            Persone(string _nom,int _age) : nom(_nom),age(_age){}
+    public :  
+            Persone(string _nom , int _age) 
+                {
+                    this->nom = _nom;
+                    this->age = _age;
+                } 
+string getNom() const  {return nom;}
+int getage() const {return age;}
+
+void setNom(string _nom) const { this->nom = _nom;}
+void setNom(int _age) const { this->nom = _age;}
+
+void afficher(){
+    cout<<"votre nom & perenom est :"<<nom<<"\net votre age est:"<<age<<endl;
+  }
+
+};
+int main()
+{
+    string nom;
+    int age;
+    cout<<"donner votre Nom et Prenom : ";
+    getline(cin,nom);
+    cout<<"donner votre age : ";
+    cin>>age;
+    cin.ignore();
+   Persone P1 = Persone(nom , age);
+   P1.afficher();
+   return 0;
+}
---
+++ C++/TP3/EX2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Livre
+{
+private :
+       string titre;
+       string auteur;
+       int  Annepublication; 
+public :
+Livre(string _titre , string _auteur , int _Annepublication : titre(_titre) , auteur(_auteur) ,Annepublication(_Annepublication)){}
+string getTitre()
+};
+
+class Bibliotheque{
+    public: int list_livre;
+
+}
