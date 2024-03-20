0ea193421cb97c29da7ed19f700426e5de9a68df
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-20 21:09:31 +0000

    LE CONCEPT DE CLASSE

--- Python/TP4/OOP.py
+++ Python/TP4/OOP.py
@@ -1,5 +1,8 @@
 class Animal :
     def __init__(self):
         print("abdellah Ennajari")
+        self.age = 0
 Animal1 = Animal()
-Animal2 = Animal()
+Animal.age = 20
+print(Animal1.age)
+# Animal2 = Animal()
