62711b44ee7f1d29c96d3363e1d64455a75b2acc
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-20 21:22:43 +0000

    LES MÉTHODES

--- Python/TP4/OOP.py
+++ Python/TP4/OOP.py
@@ -1,8 +1,12 @@
-class Animal :
-    def __init__(self):
-        print("abdellah Ennajari")
-        self.age = 0
-Animal1 = Animal()
-Animal.age = 20
-print(Animal1.age)
-# Animal2 = Animal()
+class animial:
+    def __init__(self,nom,espace,age=0):
+        self.nom = nom
+        self.espace = espace
+        self.age = age
+    def se_presenter(self):
+        print(f"Je suis {self.nom}, je mesure {self.espace} et j'ai {self.age}")
+        
+chat = animial("Minou","chat")
+chat.se_presenter()
+print(chat.__dict__)
+              
