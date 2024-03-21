de2e9acc9446b0a8e3fec392ec4c93f14418723b
Abdellah Ennajari <Abdellahennajari2018@gmail.com>
2024-03-21 01:08:01 +0000

    Les méthodes spéciales en Python, également connues sous le nom de méthodes du double souligné (ou méthodes magiques), sont des méthodes prédéfinies qui peuvent être utilisées pour personnaliser le comportement des objets dans des contextes spécifiques.

---
+++ Python/TP4/MAGIQUES.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self, nom, espece, age = 0):
+        self.nom = nom
+        self.espece = espece
+        self.age = age
+
+    def __str__(self):
+        return "Bonjour, je m'appelle {} et je suis un {} de {} ans".format(self.nom, self.espece, self.age)
+
+    def make_sound(self):
+        pass # Here, you can implement the specific sound of each animal
+
+animal1 = Animal('Lily', 'chat', 0.5)
+animal1.make_sound() # This will call the make_sound method for the animal1 object
